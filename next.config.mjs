779345aaefc458b8b0e2e6d122d884936d73cab1
jk08y <heykimutai10@gmail.com>
2025-06-24 18:38:50 +0300

    jetsy - saas

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
